f234d535138a2f7aa931b42f54782350267ce766
Sara Gorecki <sgorecki@gmail.com>
2013-11-25 13:51:16 -0500

    add hashes segment

--- ruby_assessment.rb
+++ ruby_assessment.rb
@@ -4,3 +4,10 @@ array << "Sue"
 array.each {|name| puts name}
 array[1]
 array.index("Jeff")
+
+#2. Hashes
+instructor = {:name=>"Ashley", :age=>27}
+instructor[:location] = "NYC"
+instructor.each{ |key, value| puts "#{key.to_s} is #{value}."}
+instructor[:name]
+instructor.key(27)
