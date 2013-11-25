15f9515b2c1daf7d9317b9b12e7f0427b23d29d5
Sara Gorecki <sgorecki@gmail.com>
2013-11-25 14:29:06 -0500

    add nested structures segment

--- ruby_assessment.rb
+++ ruby_assessment.rb
@@ -11,3 +11,88 @@ instructor[:location] = "NYC"
 instructor.each{ |key, value| puts "#{key.to_s} is #{value}."}
 instructor[:name]
 instructor.key(27)
+
+#3. Nested Structures
+school = { 
+  :name => "Happy Funtime School",
+  :location => "NYC",
+  :instructors => [ 
+    {:name=>"Blake", :subject=>"being awesome" },
+    {:name=>"Ashley", :subject=>"being better than blake"},
+    {:name=>"Jeff", :subject=>"karaoke"}
+  ],
+  :students => [ 
+    {:name => "Marissa", :grade => "B"},
+    {:name=>"Billy", :grade => "F"},
+    {:name => "Frank", :grade => "A"},
+    {:name => "Sophie", :grade => "C"}
+  ]
+}
+school[:founded_in] = 2013
+school[:students] << {:name => "Sally", :grade => "B"}
+def remove_student(school,name)
+	school[:students].each do |student|
+		school[:students].delete(student) if student[:name] == (name)
+	end
+end
+remove_student(school, "Billy")					#if part of a class, could call school.remove_student
+
+school[:students].each { |student| student[:semester] = "Summer"}
+
+def instructor_subject(school, name)
+	school[:instructors].each do |instructor|
+		instructor[:subject] = "being almost better than Blake" if instructor[:name] == name
+	end
+end
+instructor_subject(school, "Ashley")
+
+def change_grade(school, name, new_grade)
+	school[:students].each do |student|
+		student[:grade] = new_grade if student[:name] == name
+	end
+end
+change_grade(school, "Frank", "F")
+
+def with_grade(school, grade)
+	students = []
+	school[:students].each do |student|
+		students << student[:name] if student[:grade] == grade
+	end
+	students
+end
+with_grade(school, "B")
+
+def instructor_subject(school, name)
+	subject = ""
+	school[:instructors].each do |instructor|
+		if instructor[:name] == name
+			subject = instructor[:subject]
+		end
+	end
+	subject
+end
+instructor_subject(school, "Jeff")
+
+school.each do |key, value|
+	if value.class == Array
+		puts "The school's #{key.to_s} are:"
+		value.each do |hash|
+			hash.each do |key2, value2|
+				print "    #{key2}: #{value2}.  "
+			end
+			puts 
+		end
+	else
+		puts "The school's #{key.to_s} is #{value.to_s}."		
+	end
+end
+
+
+
+
+
+
+
+
+
+
